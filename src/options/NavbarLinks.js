8402dd7904ca7c9ca6927ebcdde78b679d3340e3
Amidou Kante <kamdiou@gmail.com>
2019-08-05 14:55:13 -0400

    delete files

--- src/options/NavbarLinks.js
+++
@@ -1,9 +0,0 @@
-export const BaseLinks = [
-    {name: 'Home', link: '/'},
-    {name: 'Designers', link: '/designers'},
-    {name: 'Clothing', link: '/clothing'},
-    {name: 'Articles', link: '/editorial'},
-    {name: 'Login', link: '/account/login', checkAuth: true},
-    {name: 'About', link: '/about'},
-    {name: 'Contact', link: '/contact-us'},
-];
